e58b783a7ae93bdea64944e9251f3abe72b3a36a
vozle <vozle2@gmail.com>
2022-02-17 16:40:44 +0300

    Code fix

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,13 +1,11 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public int squareQuantyCalc(int min, int max) {
+    public int squareQuantityCalc(int min, int max) {
         int counter = 0;
         for (int i = 10; i <= 99; i++) {
-            for (int j = min; j <= max; j++) {
-                if (i * i == j) {
-                    counter++;
-                }
+            if (i * i >= min && i * i <= max) {
+                counter++;
             }
         }
         return counter;
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -7,27 +7,34 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @Test
-    void threeSquares() {
+    void shouldSquareQuantityCalcThreeSquares() {
         SQRService service = new SQRService();
-        int excepted  = 3;
-        int actual = service.squareQuantyCalc(200, 300);
+        int excepted = 3;
+        int actual = service.squareQuantityCalc(200, 300);
         assertEquals(excepted, actual);
     }
 
     @Test
-    void zeroSquare() {
+    void shouldSquareQuantityCalcZeroSquare() {
         SQRService service = new SQRService();
-        int excepted  = 0;
-        int actual = service.squareQuantyCalc(1, 5);
+        int excepted = 0;
+        int actual = service.squareQuantityCalc(1, 5);
         assertEquals(excepted, actual);
     }
 
     @Test
-    void tenSquares() {
+    void shouldSquareQuantityCalcTenSquares() {
         SQRService service = new SQRService();
-        int excepted  = 10;
-        int actual = service.squareQuantyCalc(1, 370);
+        int excepted = 10;
+        int actual = service.squareQuantityCalc(1, 370);
         assertEquals(excepted, actual);
     }
 
+    @Test
+    void shouldSquareQuantityCalcSeventyFourSquares() {
+        SQRService service = new SQRService();
+        int excepted = 74;
+        int actual = service.squareQuantityCalc(1, 7000);
+        assertEquals(excepted, actual);
+    }
 }
